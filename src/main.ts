0a70998752b2313c3de9ffec75b7b77ac26ef6cb
Jason Jafari <me@jasonjafari.com>
2022-11-14 22:10:29 -0500

    test

--- src/main.ts
+++ src/main.ts
@@ -3,11 +3,10 @@ import { AppModule } from './app.module';
 import * as dotenv from 'dotenv';
 dotenv.config();
 
-
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const port = process.env.APP_PORT || 8403;
-  console.log(port);
-  await app.listen(port);
+  console.log(port, 'abcd');
+  await app.listen(parseInt(port));
 }
 bootstrap();
