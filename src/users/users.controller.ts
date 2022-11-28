c239a6dd198cdc55fbb004f0bd8aad96dc46f86e
Jason Jafari <me@jasonjafari.com>
2022-11-27 23:36:36 -0500

    OK

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,14 +1,11 @@
 import { Controller, Post, Request, UseGuards } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
 import { AuthService } from 'src/auth/auth.service';
-import { UsersService } from './users.service';
-// import { AuthGuard } from '@nestjs/passport';
 
 @Controller('users')
 export class UsersController {
 
     constructor(
-        private usersService: UsersService,
         private authService: AuthService
     ) { }
 
